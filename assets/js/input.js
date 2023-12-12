c9a0162fc9f6cb85fc193c7222f536f85bc53d46
ArnaudDarre <arnaudarre@gmail.com>
2023-12-12 09:50:08 +0100

    Remaining form field components

--- assets/js/input.js
+++ assets/js/input.js
@@ -1,11 +1,21 @@
-// focus on an input element on page load
-var inputElement = document.querySelector('.input--focused .input__control');
-inputElement.focus();
+// focus an input element on page load
+var inputFocusElement = document.querySelector('.input--focused .input__control');
+inputFocusElement.focus();
 
 // add 'disabled' attribute to the input element
-var inputElement = document.querySelector('.input--disabled .input__control');
-inputElement.setAttribute('disabled', 'disabled');
+var inputDisabledElement = document.querySelector('.input--disabled .input__control');
+inputDisabledElement.setAttribute('disabled', 'disabled');
 
 // add 'required' attribute to the input element
-var inputElement = document.querySelector('.input--required .input__control');
-inputElement.setAttribute('required', 'required');
+var inputRequiredElement = document.querySelector('.input--required .input__control');
+inputRequiredElement.setAttribute('required', 'required');
+
+
+
+
+
+// Update disabled and required above to add attributes when used for checkboxes and radios
+
+// Make functional, careful that the other functions still work after
+var checkboxDisabledElement = document.querySelector('.checkbox--disabled .checkbox__control');
+checkboxDisabledElement.setAttribute('disabled', 'disabled');
