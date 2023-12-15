b611138f697a4e68cc201a1c5878dba4e4827fa3
ArnaudDarre <arnaudarre@gmail.com>
2023-12-15 12:35:30 +0000

    Create button script

---
+++ assets/js/button.js
@@ -0,0 +1,3 @@
+// add 'disabled' attribute to the button element
+var buttonDisabledElement = document.querySelector('.button--disabled');
+buttonDisabledElement.setAttribute('disabled', 'disabled');
