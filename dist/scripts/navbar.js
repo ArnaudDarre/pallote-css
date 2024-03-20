fa3b40a87f8984ba1c0a311e5d68184d094de8ba
ArnaudDarre <arnaudarre@gmail.com>
2024-03-20 17:02:41 +0000

    Create sidebar responsive behaviour

--- dist/scripts/navbar.js
+++ dist/scripts/navbar.js
@@ -1,9 +1,24 @@
 const body = document.querySelector('body');
 const navbar = document.querySelector('.navbar');
-const button = document.querySelector('.button--menu');
+const button_navbar = document.querySelector('.navbar__button');
+const sidebar = document.querySelector('.sidebar');
+const button_sidebar_open = document.querySelector('.sidebar__button');
+const button_sidebar_close = document.querySelector('.sidebar__button--close');
 
-button.addEventListener('click', function () {
-  button.classList.toggle('button--menu--open');
+button_navbar.addEventListener('click', function () {
+  button_navbar.classList.toggle('js-opened');
   navbar.classList.toggle('js-opened');
-  body.classList.toggle('js-freeze');
+  body.classList.toggle('js-navbar');
+});
+
+button_sidebar_open.addEventListener('click', function () {
+  sidebar.classList.add('js-opened');
+  body.classList.add('js-sidebar');
+  button_navbar.classList.remove('js-opened');
+  navbar.classList.remove('js-opened');
+  body.classList.remove('js-navbar');
+});
+button_sidebar_close.addEventListener('click', function () {
+  sidebar.classList.remove('js-opened');
+  body.classList.remove('js-sidebar');
 });
